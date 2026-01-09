07ccfae1fdcd927ee9ddd78edb52416d0de31ec8
Nick Burgan <13688219+nmburgan@users.noreply.github.com>
2026-01-09 00:43:13 -0800

    Update example project.clj

--- example/program/project.clj
+++ example/program/project.clj
@@ -1,10 +1,10 @@
-(defproject puppetlabs/i18n-example-program "0.1.0"
+(defproject org.openvoxproject/i18n-example-program "0.1.0"
   :description "A sample use of the i18n library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.4"]
-                 [puppetlabs/i18n "0.1.0-SNAPSHOT"]]
-  :plugins      [[puppetlabs/i18n "0.1.0-SNAPSHOT"]]
+                 [org.openvoxproject/i18n "1.0.2"]]
+  :plugins      [[org.openvoxproject/i18n "1.0.2"]]
   :main puppetlabs.i18n-example-program.main
   :aot [puppetlabs.i18n-example-program.main])
